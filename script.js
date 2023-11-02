f590ee84c1505bb69b1a3dff9ed96670577325b5
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-11-02 07:15:51 +0100

    Improve the validation Ui
    Add checkmark to valid fields once they are filled in
    remove the autofill icon that appears in safari  when field are in focus

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function checkIfFilled() {
-  var fields = Array.from(document.querySelectorAll("input-field"));
+function checkPasswordEquality() {
+  var password = document.getElementById("pwd");
+  var passwordConfirmation = document.getElementById("pwd-confirm");
+  var messageOutput = document.getElementById("pwd-confirm-message");
 
-  fields.forEach((x) => {
-    for (var i = 0; i < x.length; i++) {
-      if ((x[i].value = "")) {
-        x[i].style.backgroundColor = "#f52447";
-      } else {
-        x[i].style.backgroundColor = "#24f54b";
-      }
-    }
-  });
+  if (password.value == passwordConfirmation.value) {
+    console.log("no");
+  }
 }
+
+checkPasswordEquality();
